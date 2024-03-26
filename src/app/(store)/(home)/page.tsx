d54c904f93743188d9da9894229efa1d537d396f
Tiago Muniz de Araujo <tiagomuniz130@gmail.com>
2024-03-26 19:53:16 +0000

    home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Home() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
+      <Link
+        href=""
+        className="row-sapn-6 col-span-6 overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/moletom-never-stop-learning.png"
+          className=""
+          width={860}
+          height={860}
+          alt=""
+        />
+      </Link>
     </div>
   );
 }
