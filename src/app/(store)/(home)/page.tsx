db94fd23a7e105d237378d48bf0497fa798ad85d
Tiago Araujo <tiagomuniz130@gmail.com>
2024-04-01 08:00:27 -0300

    fix: url correction

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -3,13 +3,41 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
         href="/"
-        className="col-span-6 row-span-6 overflow-hidden rounded-lg bg-zinc-900"
+        className="col-span-6 row-span-6 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           src="/moletom-never-stop-learning.png"
+          className=""
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="col-span-3 row-span-3 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/moletom-ai-side.png"
+          className=""
+          width={860}
+          height={860}
+          quality={100}
+          alt=""
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="col-span-3 row-span-3 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/moletom-java.png"
           className=""
           width={860}
           height={860}
