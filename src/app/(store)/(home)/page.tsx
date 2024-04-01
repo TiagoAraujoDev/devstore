8ca9dff595a37a535ef719425f92abdf0b43589c
Tiago Muniz de Araujo <tiagomuniz130@gmail.com>
2024-04-01 13:22:39 +0000

    feat: building the home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -6,25 +6,29 @@ export default async function Home() {
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
         href="/"
-        className="col-span-6 row-span-6 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
+        className="group relative col-span-6 row-span-6 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           src="/moletom-never-stop-learning.png"
-          className=""
+          className="transition-transform duration-500 group-hover:scale-105"
           width={860}
           height={860}
           quality={100}
           alt=""
         />
+        <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60  p-1 pl-5">
+          <span>Moletom NLW</span>
+          <span> R$ 79,99</span>
+        </div>
       </Link>
 
       <Link
         href="/"
-        className="col-span-3 row-span-3 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
+        className="group col-span-3 row-span-3 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           src="/moletom-ai-side.png"
-          className=""
+          className="transition-transform group-hover:scale-105"
           width={860}
           height={860}
           quality={100}
@@ -34,11 +38,11 @@ export default async function Home() {
 
       <Link
         href="/"
-        className="col-span-3 row-span-3 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
+        className="group col-span-3 row-span-3 flex items-center justify-end overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           src="/moletom-java.png"
-          className=""
+          className="transition-transform group-hover:scale-105"
           width={860}
           height={860}
           quality={100}
